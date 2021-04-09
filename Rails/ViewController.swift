61e1c44ade6c46052b436bd889e0ab6b0ff3e54f
Alexey Salangin <a7ls@ya.ru>
2021-04-09 15:10:21 +0300

    Initial Commit

---
+++ Rails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rails
+//
+//  Created by Alexey Salangin on 4/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
